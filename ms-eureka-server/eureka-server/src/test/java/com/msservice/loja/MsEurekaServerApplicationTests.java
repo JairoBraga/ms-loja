06d1d8386a50b848cd3aa44c0c5b53681c82512d
JairoBraga <neto.braga08@gmail.com>
2021-03-27 22:00:08 -0300

    Adicionado o apigateway e eureka server

---
+++ ms-eureka-server/eureka-server/src/test/java/com/msservice/loja/MsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msservice.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- ms-orderService/src/main/java/com/example/demo/serviceImpl/OrderServiceImpl.java
+++ ms-orderService/src/main/java/com/example/demo/serviceImpl/OrderServiceImpl.java
@@ -10,12 +10,8 @@ import com.example.demo.repository.OrderRepository;
 import com.example.demo.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
